910816caedd8f254d35bed72e9f671a8838741bc
Emily Mearns <mearnsem@gmail.com>
2016-05-18 23:28:19 -0600

    Save Button Works with Simple Core Data
    Need to practice rewriting the code a few times

--- Challenges/SimpleCoreData/SimpleCoreData/ViewController.swift
+++ Challenges/SimpleCoreData/SimpleCoreData/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let request = NSFetchRequest(entityName: "Person")
+        let moc = Stack.sharedStack.managedObjectContext
+        if let people = try? moc.executeFetchRequest(request),
+            person = people.first {
+            let name = person.valueForKey("name") as! String
+            textField.text = name
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func saveButtonTapped(sender: UIButton) {
+        let moc = Stack.sharedStack.managedObjectContext
+        let entity = NSEntityDescription.entityForName("Person", inManagedObjectContext: moc)!
+        let person = NSManagedObject(entity: entity, insertIntoManagedObjectContext: moc)
+        person.setValue(textField.text, forKey: "name")
+        let _ = try? moc.save()
+    }
 
 }
 
